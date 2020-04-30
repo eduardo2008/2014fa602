ec27df12cf91a20c42d6f94312d5525aeefc2b05
Eduardo Flores <eduroot@gmail.com>
2020-04-29 18:33:56 -0600

    primer commit

---
+++ EduardoFlores2014fa602/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EduardoFlores2014fa602.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
